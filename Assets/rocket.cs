8db1c93c685cd2b5c3612b637423058156501883
D-3-X-T-3-R <43900202+D-3-X-T-3-R@users.noreply.github.com>
2019-07-06 16:37:46 +0530

    added movement features for rocket

---
+++ Assets/rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+    Rigidbody rigid_body;
+
+	// Use this for initialization
+	void Start () {
+        rigid_body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnKeyPress();
+		
+	}
+
+    private void OnKeyPress()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigid_body.AddRelativeForce(Vector3.up);
+            print("space");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+            print("right");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+            print("left");
+        }
+    }
+}
